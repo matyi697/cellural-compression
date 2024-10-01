34a445e22ad17e7faa4fe5d55938e177bc048d44
matyi697 <wittmann.matyas@gmail.com>
2024-10-01 23:45:26 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.gitignore

--- Examples/Demo3/main.c
+++ Examples/Demo3/main.c
@@ -34,21 +34,48 @@ void rotateR(bool grid[255][255], int iteration) {
         return;
 }
 
-/*
-A grid offset egy iteracio modulo 
-ha a gridbol kiindexelunk akkor osszefuzzik az elejevel
-
-*/
+void rotateL(bool grid[255][255], int iteration) {
+    bool offset = iteration % 2; //lehet currentIteration & 1;
+    for (int y = offset; y < 255 - offset; y += 2)
+        for (int x = offset; x < 255 - offset; x += 2) {
+            //tulindexelesi problemak elkerulese
+            int x0 = x;
+            int x1 = x + 1 > 254 ? 0 : x + 1;
+            int y0 = y;
+            int y1 = y + 1 > 254 ? 0 : y + 1;
+            //a szabaly (simple rotate)
+            if(grid[x0][y0] && !grid[x1][y0] && !grid[x1][y1] && !grid[x0][y1]) {   //bal felso->bal also
+                grid[x0][y0] = 0;
+                grid[x0][y1] = 1;
+            }
+            else
+            if(!grid[x0][y0] && !grid[x1][y0] && !grid[x1][y1] && grid[x0][y1]) {   //bal also -> jobb also
+                grid[x0][y1] = 0;
+                grid[x1][y1] = 1;
+            }
+            else
+            if(!grid[x0][y0] && !grid[x1][y0] && grid[x1][y1] && !grid[x0][y1]) {   //jobb also -> jobb felso
+                grid[x1][y1] = 0;
+                grid[x1][y0] = 1;
+            }
+            else
+            if(!grid[x0][y0] && grid[x1][y0] && !grid[x1][y1] && !grid[x0][y1]) {   //jobb felso -> bal felso
+                grid[x1][y0] = 0;
+                grid[x0][y0] = 1;
+            }
+        }
+    return;
+}
 
 void drawRect(bool grid[255][255], int x0, int y0, int x1, int y1) {
     for(int x = x0; x <= x1; x++) {
-        grid[x][y0] = 1; // Felső vonal
-        grid[x][y1] = 1; // Alsó vonal
+        grid[x][y0] = 1;
+        grid[x][y1] = 1;
     }
 
     for(int y = y0; y <= y1; y++) {
-        grid[x0][y] = 1; // Bal oldali vonal
-        grid[x1][y] = 1; // Jobb oldali vonal
+        grid[x0][y] = 1;
+        grid[x1][y] = 1;
     }
 }
 
@@ -68,8 +95,13 @@ int main() {
 
     drawRect(grid, 10, 10, 14, 14);
     writeGridToFile("origin.txt", grid);
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 10; i++) {
         rotateR(grid, i);
-    writeGridToFile("result.txt", grid);
+    }
+    writeGridToFile("rotated.txt", grid);
+
+    for (int i = 9; i >= 0; i--)
+        rotateL(grid, i);
+    writeGridToFile("recovered", grid);
     return 0;
 }

e8ddc9a1ce768c91b0fb728cb453d0dced67eec0
matyi697 <wittmann.matyas@gmail.com>
2024-10-03 16:17:32 +0200

    Merge remote-tracking branch 'origin/main'  Conflicts: 	.gitignore

--- Examples/Demo3/main.c
+++ Examples/Demo3/main.c
@@ -7,54 +7,7 @@
 #define MAX_DEPTH 100   //a legkedvezobb tomoritesi arany eleresehez valo max melyseg
 #define MAX_X 50        //a lenyeg hogy 2 hatvany legyen
 #define MAX_Y 50        //a lenyeg hogy 2 hatvany legyen
-#define SET_SIZE 4      //hany rule van
-
-void rotate(bool grid[MAX_X][MAX_Y], int iteration, char *direction) {
-    bool offset = iteration % 2; // lehet currentIteration & 1
-    for (int y = offset; y < MAX_Y - offset; y += 2) {
-        for (int x = offset; x < MAX_X - offset; x += 2) {
-            // Túlindexelési problémák elkerülése
-            int x0 = x;
-            int x1 = x + 1 > MAX_X ? 0 : x + 1;
-            int y0 = y;
-            int y1 = y + 1 > MAX_Y ? 0 : y + 1;
-
-            if (strcmp(direction, "right")) {
-                if (grid[x0][y0] && !grid[x1][y0] && !grid[x1][y1] && !grid[x0][y1]) {
-                    grid[x0][y0] = 0;
-                    grid[x1][y0] = 1;
-                } else if (!grid[x0][y0] && grid[x1][y0] && !grid[x1][y1] && !grid[x0][y1]) {
-                    grid[x1][y0] = 0;
-                    grid[x1][y1] = 1;
-                } else if (!grid[x0][y0] && !grid[x1][y0] && grid[x1][y1] && !grid[x0][y1]) {
-                    grid[x1][y1] = 0;
-                    grid[x0][y1] = 1;
-                } else if (!grid[x0][y0] && !grid[x1][y0] && !grid[x1][y1] && grid[x0][y1]) {
-                    grid[x0][y1] = 0;
-                    grid[x0][y0] = 1;
-                }
-            } else if (strcmp(direction, "left")) {
-                if (grid[x0][y0] && !grid[x1][y0] && !grid[x1][y1] && !grid[x0][y1]) {
-                    grid[x0][y0] = 0;
-                    grid[x0][y1] = 1;
-                } else if (!grid[x0][y0] && !grid[x1][y0] && !grid[x1][y1] && grid[x0][y1]) {
-                    grid[x0][y1] = 0;
-                    grid[x1][y1] = 1;
-                } else if (!grid[x0][y0] && !grid[x1][y0] && grid[x1][y1] && !grid[x0][y1]) {
-                    grid[x1][y1] = 0;
-                    grid[x1][y0] = 1;
-                } else if (!grid[x0][y0] && grid[x1][y0] && !grid[x1][y1] && !grid[x0][y1]) {
-                    grid[x1][y0] = 0;
-                    grid[x0][y0] = 1;
-                }
-            } else {
-                perror("Nem helyes irany a forgatasra!\n");
-                return;
-            }
-        }
-    }
-    return;
-}
+#define SET_SIZE 6      //hany rule van
 
 void drawRect(bool grid[MAX_X][MAX_Y], int x0, int y0, int x1, int y1) {
     for (int x = x0; x <= x1; x++) {
@@ -91,16 +44,6 @@ void printGrid(bool grid[MAX_X][MAX_Y]) {
             printf(" %c", grid[x][y] == 1 ? 'O' : ' ');
         printf("\n");
     }
-    return;
-}
-
-void delay(int milli_seconds)
-{
-    milli_seconds *= 1000;
-
-    clock_t start_time = clock();
-
-    while (clock() < start_time + milli_seconds);
     return;
 }
 
@@ -144,46 +87,28 @@ void readRule(char* filename, unsigned rules[SET_SIZE][2]) {
     return;
 }
 
-void TEST1(void) {
-    bool test[MAX_X][MAX_Y] = {0};
-    drawRect(test, 20, 20, 25, 28);
-    writeGridToFile("original.txt", test);
-
-    rotate(test, 1, "left");
-
-    writeGridToFile("rotated_good.txt", test);
+void randomizeRect(bool grid[MAX_X][MAX_Y], int desinty, int x0, int y0, int x1, int y1) {
+    srand(time(0));
+    for (int y = y0; y < y1; y++)
+        for(int x = x0; x < x1; x++)
+            grid[x][y] = (rand() % desinty == 1) ? 1 : 0;
     return;
 }
 
-void TEST2 (void) {
-    bool test[MAX_X][MAX_Y] = {0};
-    srand(time(0));
-    for (int y = 10; y < 20; y++)
-        for (int x = 10; x < 20; x++)
-            test[x][y] = rand() % 4 == 1 ? 1 : 0;
-
-    while (1) {
-        bool i = 0;
-        rotate(test, i, "right");
-        system("clear");
-        printGrid(test);
-        delay(500);
-    }
-}
-
-void TEST3(void) {
-    unsigned rules[4][2] = {0};
-    bool test[MAX_X][MAX_Y] = {0};
-    readRule("/home/wittmann/Programing/Algoritmusok ás Adatszerkezetek hatékony implementálása C nyelven/Házi Feladat/cellural-compression/Examples/Demo3/rules.txt", rules);
-    drawRect(test, 20, 20, 25, 28);
-
-    writeGridToFile("original.txt", test);
-    generalisedRuleSet(rules, test, 1);
-    writeGridToFile("rotated.txt", test);
+void TEST() {
+    unsigned rules[SET_SIZE][2] = {};
+    bool grid[MAX_X][MAX_Y] = {0};
+    readRule("/home/wittmann/Programing/Algoritmusok ás Adatszerkezetek hatékony implementálása C nyelven/Házi Feladat/cellural-compression/Examples/Demo3/stringThing.txt", rules);
+    randomizeRect(grid, 4, 20, 20, 40, 40);
+    writeGridToFile("s_test_in.txt", grid);
+    generalisedRuleSet(rules, grid, 1);
+    writeGridToFile("s_test_out.txt", grid);
+    generalisedRuleSet(rules, grid, 1);
+    writeGridToFile("s_test_rec.txt", grid);
+    return;
 }
 
 int main(int argc, char *argv[]) {
-    TEST1();
-    TEST3();
+    TEST();
     return 0;
 }
